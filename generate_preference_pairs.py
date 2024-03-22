287fa984236470e05a29c864b3f59813b5ccbbb3
Qiyao Wei <juventia2014@gmail.com>
2024-03-22 09:14:39 +0000

    Update generate_preference_pairs.py

--- generate_preference_pairs.py
+++ generate_preference_pairs.py
@@ -3,7 +3,7 @@ warnings.filterwarnings('ignore', category=UserWarning, message='TypedStorage is
 import os
 os.environ["WANDB__SERVICE_WAIT"]="6000"
 
-import json
+# Step 1:
 import pandas as pd
 import random
 random.seed("1234")
@@ -13,14 +13,8 @@ from datasets import load_dataset
 from torch.utils.data import DataLoader
 from trl.core import LengthSampler
 
-# I chose to use this model to avoid SFT
 model_name="lvwerra/gpt2-imdb"
-
-# I used the batch=1 setting to avoid complications. IIRC, gpt-2 does not have a padding scheme, so it would be good to avoid having to deal with padding during generation
 batch_size = 1
-
-# Everything else should be the same as the paper
-# This implementation is also heavily adapted from the gpt-2 example notebooks in trl
 model = AutoModelForCausalLM.from_pretrained(model_name).to("cuda")
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 tokenizer.pad_token = tokenizer.eos_token
@@ -28,6 +22,18 @@ reward_pipe = pipeline("text-classification", model="siebert/sentiment-roberta-l
 reward_kwargs = {"top_k": None, "function_to_apply": "none", "batch_size": 16}
 
 def build_dataset(dataset_name="imdb", input_min_text_length=2, input_max_text_length=8):
+    """
+    Build dataset for training. This builds the dataset from `load_dataset`, one should
+    customize this function to train the model on its own dataset.
+
+    Args:
+        dataset_name (`str`):
+            The name of the dataset to be loaded.
+
+    Returns:
+        dataloader (`torch.utils.data.DataLoader`):
+            The dataloader for the dataset.
+    """
 
     ds = load_dataset(dataset_name, split="train")
     ds = ds.rename_columns({"text": "review"})
@@ -51,6 +57,7 @@ print(len(dataset))
 gen_kwargs = {"min_length": -1, "top_k": 0.0, "top_p": 1.0, "do_sample": True, "pad_token_id": tokenizer.eos_token_id, "max_new_tokens": 20}
 
 final = list()
+# TODO: Check padding, although I don't think gpt2 has padding?
 def outputs_and_rewards(query, query_tensor, temperature):
     
     output = model.generate(query_tensor, temperature=temperature, **gen_kwargs).squeeze()[-gen_kwargs["max_new_tokens"]:]
@@ -69,13 +76,26 @@ def outputs_and_rewards(query, query_tensor, temperature):
 
 for batch in dataloader:
     df = dict()
-    df["query"] = batch["query"]
+    df["query"] = batch["query"][0]
     query_tensor = batch["input_ids"].to("cuda")
 
     response1, rewards1 = outputs_and_rewards(df["query"], query_tensor, 0.2)
     response2, rewards2 = outputs_and_rewards(df["query"], query_tensor, 0.4)
     response3, rewards3 = outputs_and_rewards(df["query"], query_tensor, 0.6)
     response4, rewards4 = outputs_and_rewards(df["query"], query_tensor, 0.8)
+    
+    assert type(df["query"]) == type("")
+    assert type(response1) == type("")
+    assert type(response1) == type("")
+    assert type(response2) == type("")
+    assert type(response3) == type("")
+    assert type(response4) == type("")
+    assert type(rewards1) == type(0.0)
+    assert type(rewards2) == type(0.0)
+    assert type(rewards3) == type(0.0)
+    assert type(rewards4) == type(0.0)
+    
+    
     df["response1"] = response1
     df["reward1"] = rewards1
     df["response2"] = response2
@@ -89,7 +109,13 @@ for batch in dataloader:
 df_results = pd.DataFrame(final)
 df_results.to_csv("results.csv", index=False)
 
-
+# Step 2:
+import json
+import random
+random.seed("1234")
+from transformers import set_seed
+set_seed(1234)
+import pandas as pd
 final = list()
 data = pd.read_csv("results.csv")
 
@@ -109,7 +135,6 @@ for i in range(len(data)):
     final.append({"prompt": data["query"][i], "chosen": d[indices[1]], "rejected": d[indices[2]]})
     final.append({"prompt": data["query"][i], "chosen": d[indices[1]], "rejected": d[indices[3]]})
     final.append({"prompt": data["query"][i], "chosen": d[indices[2]], "rejected": d[indices[3]]})
-
-# These are your final preference pairs, there should be ~150k of them
-with open('data.json', 'w', encoding='utf-8') as f:
+    
+with open('test.json', 'w', encoding='utf-8') as f:
     json.dump(final, f, ensure_ascii=False, indent=4)
